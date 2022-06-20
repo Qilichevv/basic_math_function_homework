f90fe57de9c4fb35bdb1c4b79b59ad89ec986aa1
qilichevv <behruzqilichevv23@gmail.com>
2022-06-20 13:23:36 +0300

    basic12

--- basic_func12.py
+++ basic_func12.py
@@ -1,4 +1,13 @@
 # Create a function called main.
+from calendar import c
+
+
+def main(a):
 # Create function argument a.
 # Return the value of a.
+
+ return a
 # Round the result to 2 decimal places.
+a=5.5855
+
+print( round(a,2))
