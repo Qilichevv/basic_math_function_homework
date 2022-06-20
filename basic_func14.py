398f7125ff888c93b7ee66571a23abc84065f5d7
qilichevv <behruzqilichevv23@gmail.com>
2022-06-20 14:15:21 +0300

    func14

--- basic_func14.py
+++ basic_func14.py
@@ -1,3 +1,7 @@
+from math import floor
 # Create a function called main.
 # Create function arguments a and b.
 # Returns the floor division of a and b.
+def main(a,b) :
+    a,b=floor
+    return a
